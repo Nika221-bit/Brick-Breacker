1ef23761eb3ccd7c6b1f203cab733a19d9b57507
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-20 23:46:36 +0400

    walls

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   let elements = {
-    Game: document.querySelector(".game")
-    
+    Game: document.querySelector(".game"),
+    desk: document.querySelector('.desk'),
+    wallContainer: document.querySelector('.walls') // სწორად
   };
-  let styleroot = document.querySelector(':root')
+
+  let styleroot = document.querySelector(':root');
   let gamewidth = elements.Game.getBoundingClientRect().width;
   let gameheight = elements.Game.getBoundingClientRect().height;
+  let walllenght = 40;
 
   defData();
 
   function defData() {
     mouseMove();
+    createWall(); // რომ კედლები შეიქმნას
   }
 
   function mouseMove() {
     elements.Game.addEventListener('mousemove', (e) => {
-      console.log(e.offsetX, e.offsetY);
+      setStyleProperty('--LeftDesk', `${e.offsetX}px`);
     });
   }
-  function setStyleProperty(variable,value){
-    styleroot.style.setProperty(variable,value)
+
+  function setStyleProperty(variable, value) {
+    styleroot.style.setProperty(variable, value);
   }
-  function getStyleProperty(variable){
-    return getComputedStyle(styleroot).getPropertyValue(variable)
+
+  function getStyleProperty(variable) {
+    return parseFloat(getComputedStyle(styleroot).getPropertyValue(variable));
+  }
+
+  function createWall() {
+    for (let i = 0; i < walllenght; i++) {
+      let elem = document.createElement('div');
+      elem.classList.add('wall');
+      elements.wallContainer.appendChild(elem);
+    }
   }
 });
