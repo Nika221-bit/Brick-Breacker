6cda44c184528bba69cced5770d43427a5827aa7
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-21 23:00:12 +0400

    2

--- main.js
+++ main.js
@@ -3,16 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
   let elements = {
     Game: document.querySelector(".game"),
     desk: document.querySelector('.desk'),
-    wallContainer: document.querySelector('.walls') // სწორად
+    wallContainer: document.querySelector('.walls') ,
+    ball:document.querySelector('.ball'),
   };
 
   let styleroot = document.querySelector(':root');
   let gamewidth = elements.Game.getBoundingClientRect().width;
   let gameheight = elements.Game.getBoundingClientRect().height;
   let walllenght = 40;
-
+  let ballphisycs = {
+    x:getStyleProperty('--Leftball'),
+    y:getStyleProperty('--Topball'),
+    w:elements.ball.getBoundingClientRect().width,
+    h:elements.ball.getBoundingClientRect().height,
+    vx:3,
+    vy:3,
+    spped:1.5,
+  }
   defData();
+  function update(){
+    console.log('hello')
+    requestAnimationFrame(update)
 
+  }
+ 
   function defData() {
     mouseMove();
     createWall(); // რომ კედლები შეიქმნას
